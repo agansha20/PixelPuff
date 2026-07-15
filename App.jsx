aca9e940ca9a3f401171f6c962c5e61d42ed933a
Agansha <aganshat007@gmail.com>
2026-07-15 22:26:18 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Login from './Components/Login.jsx';
 import Home from './Components/Home.jsx';
 import Shop from './Components/Shop.jsx';
@@ -7,10 +8,13 @@ import Contact from './Components/contact.jsx';
 import Product from './Components/Product.jsx';
 import Cart from './Components/Cart.jsx';
 
+
 function App() {
+
   return (
     <BrowserRouter basename="/PixelPuff">
       <Routes>
+
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
@@ -18,9 +22,10 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/product" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
+
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
 export default App;
